1e4399128ef17de814906967d3058b3d927b8f1f
SYDSuhail-coder <suhail.ali@playboxtv.in>
2026-04-20 18:14:19 +0530

    fix mongodb atlas connection

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-let ip = "127.0.0.1";
-let dbUrl = `mongodb://${ip}:27017/mumbail-Island`;
+// let ip = "127.0.0.1";
+// let dbUrl = `mongodb://${ip}:27017/mumbail-Island`;
+let dbUrl = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/mumbail-Island";
+
 mongoose.connect(dbUrl).then(() => {
   console.log('connection successful');
   require('@model/addLanguage');
